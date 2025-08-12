c1acf6b7136d1fef4ba3ca725229d40dce4008e7
Morgana-S <st35giv9@students.codeinstitute.net>
2025-08-12 12:03:51 +0100

    Create slug on save for records model and create Genre model

--- records/models.py
+++ records/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
+from django.utils.text import slugify
 from cloudinary.models import CloudinaryField
+from colorfield.fields import ColorField
 # Create your models here.
 
 
@@ -12,3 +14,20 @@ class Artist(models.Model):
         'image', default='default-artist_fw2mea')
     debut_year = models.PositiveIntegerField(null=True, blank=True)
     bio = models.CharField(max_length=500, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    color = ColorField(format='hex')
+    description = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.name
