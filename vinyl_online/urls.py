68e425767f2b4d3cc97c49da0a3772f89d4156a8
Morgana-S <st35giv9@students.codeinstitute.net>
2025-08-14 14:41:20 +0100

    Include records urls

--- vinyl_online/urls.py
+++ vinyl_online/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('records.urls'), name='records-urls'),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
 ]
