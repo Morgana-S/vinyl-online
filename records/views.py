be970c0f1559cea7fab92e1af3906dea06d4158b
Morgana-S <st35giv9@students.codeinstitute.net>
2025-08-21 14:03:26 +0100

    Add options for sorting and pagination to search_records_view

--- records/views.py
+++ records/views.py
@@ -35,23 +35,51 @@ def search_records_view(request):
     input on the search bar. Then displays both artists and records that match
     the search terms.
     """
+    
+    SORT_OPTIONS_ARTISTS = {
+        'name_asc': 'name',
+        'name_desc': '-name',
+        'debut_asc': 'debut_year',
+        'debut_desc': '-debut_year',
+    }
+
+    SORT_OPTIONS_RECORDS = {
+        'title_asc': 'title',
+        'title_desc': '-title',
+        'release_year_asc': 'release_year',
+        'release_year_desc': '-release_year',
+        'price_asc': 'price',
+        'price_desc': '-price'
+    }
+
+    sort_by_artists = request.GET.get('sort_artist', 'name')
+    sort_by_records = request.GET.get('sort_record', 'title')
+
+    order_field_artists = SORT_OPTIONS_ARTISTS.get(sort_by_artists, 'name')
+    order_field_records = SORT_OPTIONS_RECORDS.get(sort_by_records, 'title')
 
     query = request.GET.get('q', '')
     artists = Artist.objects.filter(Q(name__icontains=query)).order_by(
-        'name'
+        order_field_artists
     )
     records = Record.objects.filter(Q(title__icontains=query)).order_by(
-        'title'
+        order_field_records
     )
 
-    paginator = Paginator(records, 8)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    paginator_artists = Paginator(artists, 8)
+    artists_page_number = request.GET.get('artists_page')
+    artists_page_obj = paginator_artists.get_page(artists_page_number)
+
+    paginator_records = Paginator(records, 8)
+    records_page_number = request.GET.get('records_page')
+    records_page_obj = paginator_records.get_page(records_page_number)
 
     context = {
         'query': query,
         'artists': artists,
-        'results': page_obj,
+        'records': records,
+        'artists_results': artists_page_obj,
+        'records_results': records_page_obj,
     }
 
-    return render(request,'records/search.html', context)
+    return render(request, 'records/search.html', context)
