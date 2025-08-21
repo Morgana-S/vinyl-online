ecd7dd57e7eef364ccf9a81a478716e075caadf6
Morgana-S <st35giv9@students.codeinstitute.net>
2025-08-21 11:36:01 +0100

    create search_records_view

--- records/views.py
+++ records/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.db.models import Q
+from django.core.paginator import Paginator
 from .models import Record, Artist, RecordImage, Genre
 
 # Create your views here.
+
 
 def index_view(request):
     """
@@ -16,7 +19,7 @@ def index_view(request):
     featured_genre = 'Pop'
     featured_genre_records = Record.objects.filter(
         genre__name='Pop').order_by('-release_year')[:5]
-    
+
     context = {
         'latest_releases': latest_releases,
         'featured_genre': featured_genre,
@@ -24,3 +27,31 @@ def index_view(request):
     }
 
     return render(request, 'records/index.html', context)
+
+
+def search_records_view(request):
+    """
+    View for searching for records. Obtains the query information from the
+    input on the search bar. Then displays both artists and records that match
+    the search terms.
+    """
+
+    query = request.GET.get('q', '')
+    artists = Artist.objects.filter(Q(name__icontains=query)).order_by(
+        'name'
+    )
+    records = Record.objects.filter(Q(title__icontains=query)).order_by(
+        'title'
+    )
+
+    paginator = Paginator(records, 8)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'query': query,
+        'artists': artists,
+        'results': page_obj,
+    }
+
+    return render(request,'records/search.html', context)
