ad6394b1a24eaa123c831369c33afb3a9d138e7c
Morgana-S <st35giv9@students.codeinstitute.net>
2025-08-26 16:51:51 +0100

    Add allauth links to accounts

--- vinyl_online/urls.py
+++ vinyl_online/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('', include('records.urls'), name='records-urls'),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('accounts/', include('allauth.urls'))
 ]
