fbe5a5e121b02d460a66009db6f2529a6a5f2583
Morgana-S <st35giv9@students.codeinstitute.net>
2025-08-28 14:36:52 +0100

    Create url for create/edit profile

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.user_profile_view, name='profile'),
+    path('create-edit-profile/',
+         views.create_edit_profile_view,
+         name='create_edit_profile'),
+
 ]
