0566906f7652de7c15038cb336425eb96f906a11
Morgana-S <st35giv9@students.codeinstitute.net>
2025-08-28 16:38:06 +0100

    create urls for add/edit/delete delivery addresses

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,5 +6,13 @@ urlpatterns = [
     path('create-edit-profile/',
          views.create_edit_profile_view,
          name='create_edit_profile'),
-
+    path('add-delivery-address/',
+         views.create_delivery_address_view,
+         name='add_delivery_address'),
+    path('edit-delivery-address/<uuid:pk>/',
+         views.edit_delivery_address_view,
+         name='edit_delivery_address'),
+    path('delete-delivery-address/<uuid:pk>/',
+         views.delete_delivery_address_view,
+         name='delete_delivery_address')
 ]
