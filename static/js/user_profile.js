f47a4f57104408a2fd624b659bdebfbb75b8d131
Morgana-S <st35giv9@students.codeinstitute.net>
2025-08-28 16:39:26 +0100

    Add custom js for profile page

---
+++ static/js/user_profile.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+	deleteAddressButtonInit();
+});
+
+function deleteAddressButtonInit() {
+	const deleteButtons = document.querySelectorAll('.delete-button');
+	const deleteModal = new bootstrap.Modal(
+		document.getElementById('deleteAddressModal')
+	);
+	const deleteForm = document.getElementById('deleteAddressForm');
+
+	deleteButtons.forEach((btn) => {
+		btn.addEventListener('click', function () {
+            console.log('clicked delete')
+			const actionUrl = btn.dataset.url;
+			deleteForm.setAttribute('action', actionUrl);
+			deleteModal.show();
+		});
+	});
+}
