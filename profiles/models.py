905d86bd97c19d32568d520d7038fabe2eafa4e8
Morgana-S <st35giv9@students.codeinstitute.net>
2025-08-28 12:54:44 +0100

    Create and migrate the UserProfile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,11 +1,15 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinLengthValidator
 from allauth.account.models import EmailAddress
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 
 
 class UserProfile(models.Model):
+    UUID = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='profile')
     first_name = models.CharField(
@@ -13,16 +17,19 @@ class UserProfile(models.Model):
     last_name = models.CharField(
         max_length=20, validators=[MinLengthValidator(1)])
 
+    contact_phone_number = PhoneNumberField(region=None)
+
+    contact_email = models.EmailField(
+        help_text='If this is the same as the email you made the account with'
+        ', you can leave this blank.', blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.contact_email:
+            self.contact_email = self.user.email
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.user.username
 
     def full_name(self):
         return self.first_name + ' ' + self.last_name
-
-    @property
-    def contact_email(self):
-        # Obtains the user's contact email from their primary email via allauth
-        try:
-            return EmailAddress.objects.get(user=self.user, primary=True.email)
-        except EmailAddress.DoesNotExist:
-            return self.user.email
