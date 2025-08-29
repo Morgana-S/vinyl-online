aad92fe9f18bc1c65121d97983eca3af75c78fd7
Morgana-S <st35giv9@students.codeinstitute.net>
2025-08-29 14:47:10 +0100

    Add urls for newsletter and about page

--- community/urls.py
+++ community/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
          views.create_support_ticket_view, name='create_support_ticket'),
     path('support/ticket/<uuid:pk>',
          views.ticket_detail_view, name='ticket_detail'),
+    path('who-we-are/', views.about_page_view, name='about'),
+    path('newsletter/', views.newsletter_subscribe_view, name='newsletter')
 
 ]
