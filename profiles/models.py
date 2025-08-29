fc614608caa83eb75dd1a0d9eb87899b11889bbe
Morgana-S <st35giv9@students.codeinstitute.net>
2025-08-29 13:09:15 +0100

    Change userprofile to one-to-one as the user should only have one profile

--- profiles/models.py
+++ profiles/models.py
@@ -11,7 +11,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 class UserProfile(models.Model):
     UUID = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    user = models.ForeignKey(
+    user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name='profile')
     first_name = models.CharField(
         max_length=20, validators=[MinLengthValidator(1)])
