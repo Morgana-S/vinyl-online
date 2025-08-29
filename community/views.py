89dd982571cca7199ce3440b12644029e7110dd0
Morgana-S <st35giv9@students.codeinstitute.net>
2025-08-29 13:05:31 +0100

    Create create_support_ticket and ticket_detail views

---
+++ community/views.py
@@ -0,0 +1,94 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import SupportTicketForm
+from .models import SupportTicket
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.utils.html import strip_tags
+from django.template.loader import render_to_string
+# Create your views here.
+
+
+def create_support_ticket_view(request):
+    """
+    View for creating a support ticket. If the user
+    """
+
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = SupportTicketForm(request.POST, user=request.user)
+            if form.is_valid():
+                ticket = form.save(commit=False)
+                ticket.user = request.user
+                ticket.save()
+
+                # Confirmation Email
+                subject = f'Vinyl Online - Support Ticket Ref. {ticket.pk}'
+                html_message = render_to_string(
+                    'emails/support_ticket_confirmation.html',
+                    {'ticket': ticket, 'user': request.user}
+                )
+                plain_message = strip_tags(html_message)
+
+                send_mail(
+                    subject,
+                    plain_message,
+                    None,
+                    [ticket.email],
+                    html_message=html_message
+                )
+
+                messages.success(request,
+                                 'Your support ticket has now been submitted.'
+                                 ' You will receive an email with your ticket'
+                                 ' details.')
+                return redirect('profile')
+        else:
+            form = SupportTicketForm(user=request.user)
+    else:
+        if request.method == 'POST':
+            form = SupportTicketForm(request.POST)
+            if form.is_valid():
+                ticket = form.save()
+
+                # Confirmation Email
+                subject = f'Vinyl Online - Support Ticket Ref. {ticket.pk}'
+                html_message = render_to_string(
+                    'emails/support_ticket_confirmation.html',
+                    {'ticket': ticket, 'user': None}
+                )
+                plain_message = strip_tags(html_message)
+
+                send_mail(
+                    subject,
+                    plain_message,
+                    None,
+                    [ticket.email],
+                    html_message=html_message
+                )
+
+                messages.success(request,
+                                 'Your support ticket has now been submitted.'
+                                 ' You will receive an email with your ticket'
+                                 ' details.')
+                return redirect('index')
+        else:
+            form = SupportTicketForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'community/create_support_ticket.html', context)
+
+
+def ticket_detail_view(request, pk):
+    """
+    View for seeing support ticket details.
+    """
+    ticket = get_object_or_404(SupportTicket, pk=pk)
+
+    context = {
+        'ticket': ticket
+    }
+
+    return render(request, 'community/ticket_detail.html', context)
