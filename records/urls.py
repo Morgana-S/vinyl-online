65b4c53300b5de46ea892f7b22f06815b6091bb0
Morgana-S <st35giv9@students.codeinstitute.net>
2025-08-30 16:35:34 +0100

    Add edit-record url

--- records/urls.py
+++ records/urls.py
@@ -6,15 +6,17 @@ urlpatterns = [
     path('search/', views.search_records_view, name='search'),
     path('async-search/', views.search_records_ajax, name='async_search'),
     path(
-        'record/<slug:record_slug>',
+        'record/<slug:record_slug>/',
         views.record_detail_view,
-        name='record_detail'
-        ),
-    path('artist/<slug:artist_slug>',
+        name='record_detail'),
+    path('edit-record/<slug:record_slug>/',
+         views.edit_record_view,
+         name='edit_record'),
+    path('artist/<slug:artist_slug>/',
          views.artist_detail_view,
          name='artist_detail'
          ),
-    path('genre/<slug:genre_name>',
+    path('genre/<slug:genre_name>/',
          views.browse_by_genre_view,
          name='browse_by_genre'),
     path('all-records/', views.all_records_view, name='all_records'),
