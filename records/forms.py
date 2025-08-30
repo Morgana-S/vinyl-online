a36e81767a2b43da136fe19a62575795c36839c9
Morgana-S <st35giv9@students.codeinstitute.net>
2025-08-30 16:34:08 +0100

    Add EditRecordForm and image formset

---
+++ records/forms.py
@@ -0,0 +1,49 @@
+from django import forms
+from django.forms import inlineformset_factory
+from django_summernote.widgets import SummernoteWidget
+from .models import Record, RecordImage, Artist
+
+
+class EditRecordForm(forms.ModelForm):
+    """
+    Form for editing record details.
+    """
+    class Meta:
+        model = Record
+        fields = [
+            'title', 'slug', 'artist', 'release_year', 'genre', 'size', 'rpm',
+            'description', 'price', 'quantity', 'hidden'
+        ]
+        labels = {
+            'title': 'Title*',
+            'slug': 'Slug',
+            'artist': 'Artist*',
+            'release_year': 'Release Year',
+            'genre': 'Genres',
+            'size': 'Record Size*',
+            'rpm': 'Record RPM*',
+            'description': 'Record Details',
+            'price': 'Price*',
+            'quantity': 'Stock Available*',
+            'hidden': 'Hide Record from Sale?'
+        }
+        widgets = {
+            'description': SummernoteWidget()
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field.widget.__class__.__name__ != 'CheckboxInput':
+                field.widget.attrs['class'] = 'form-control'
+            else:
+                field.widget.attrs['class'] = 'form-check-input'
+
+
+RecordImageFormSet = inlineformset_factory(
+    Record,
+    RecordImage,
+    fields=('image', 'image_type'),
+    extra=1,
+    can_delete=True
+)
