9e16b9d58e95c13c8f33c892a13b057f8850c148
Morgana-S <st35giv9@students.codeinstitute.net>
2025-09-01 10:53:13 +0100

    Remove unused emailaddress field

--- profiles/models.py
+++ profiles/models.py
@@ -2,7 +2,6 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinLengthValidator
-from allauth.account.models import EmailAddress
 from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
