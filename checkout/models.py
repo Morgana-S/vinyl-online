ffb40371646784faa80e2d13a441044981dcf4b7
Morgana-S <st35giv9@students.codeinstitute.net>
2025-09-01 11:03:20 +0100

    Create OrderItem model

--- checkout/models.py
+++ checkout/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 from phonenumber_field.modelfields import PhoneNumberField
+from records.models import Record
 # Create your models here.
 
 
@@ -38,4 +39,39 @@ class Order(models.Model):
     grand_total_cost = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
-        return self.uuid
+        return f'{self.uuid} | Total: {self.grand_total_cost}'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE, related_name='items')
+    record = models.ForeignKey(
+        Record,
+        null=False,
+        blank=False)
+    quantity = models.PositiveSmallIntegerField(
+        null=False,
+        blank=False,
+        default=0,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(9),
+        ]
+    )
+    item_total = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=False,
+        blank=False,
+        editable=False
+    )
+
+    def __str__(self):
+        return f'({self.record.name} x {self.quantity} - {self.item_total})'
+
+    def save(self, *args, **kwargs):
+        self.item_total = self.record.price * self.quantity
+        super().save(*args, *kwargs)
