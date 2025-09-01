6dd4887502b5b14bc12b3ade628c68cd349fc0c6
Morgana-S <st35giv9@students.codeinstitute.net>
2025-09-01 15:22:43 +0100

    Add add-to-basket url

--- basket/urls.py
+++ basket/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_basket, name='view_basket')
+    path('', views.view_basket, name='view_basket'),
+    path('add-to-basket/', views.add_to_basket_async, name='add_to_basket')
 ]
