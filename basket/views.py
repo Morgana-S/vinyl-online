2b9c5d54de74c6e9404b726c35a8f70e3c68749c
Morgana-S <st35giv9@students.codeinstitute.net>
2025-09-01 11:59:36 +0100

    Add basic basket view and url

--- basket/views.py
+++ basket/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def view_basket(request):
+    """
+    View for viewing the total items in the shopping basket.
+    """
+    return render(request, 'basket/view_basket.html')
