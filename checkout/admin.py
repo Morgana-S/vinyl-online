6113e115fa2c3a0d4e7195e963da3431d8392890
Morgana-S <st35giv9@students.codeinstitute.net>
2025-09-02 13:56:07 +0100

    Register models

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Order, OrderItem
 # Register your models here.
+
+
+class OrderItemAdminInline(admin.TabularInline):
+    model = OrderItem
+    readonly_fields = ('item_total',)
+
+
+@admin.register(Order)
+class OrderAdmin(SummernoteModelAdmin):
+    list_display = (
+        'uuid', 'created_at', 'status', 'user', 'full_name', 'email')
+    search_fields = [
+        'uuid', 'status', 'user__username', 'full_name', 'email',
+        'phone_number']
+    # Leaving address fields open so delivery address can be changed
+    # as part of support operations
+    readonly_fields = ('uuid', 'created_at', 'user', 'full_name',)
+    list_filter = ('status',)
+    ordering = ('-created_at',)
+    inlines = (OrderItemAdminInline,)
