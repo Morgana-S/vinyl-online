1bf688a8428eeed23f4c2889d89c2658fe305506
Morgana-S <st35giv9@students.codeinstitute.net>
2025-09-02 10:03:26 +0100

    Reformat add-to-basket and create remove-from-basket urls

--- basket/urls.py
+++ basket/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
-    path('add-to-basket/', views.add_to_basket_async, name='add_to_basket')
+    path('add-to-basket/',
+         views.add_to_basket_async_view,
+         name='add_to_basket'),
+    path('remove-from-basket/<int:record_id>/',
+         views.remove_from_basket_view,
+         name='remove_basket_item')
+
 ]
