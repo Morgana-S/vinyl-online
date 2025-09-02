6ab3c584b2279e9e47295f180adc27d401fa3828
Morgana-S <st35giv9@students.codeinstitute.net>
2025-09-02 13:58:41 +0100

    Add checkout urls

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.checkout_view, name='checkout'),
+    path('order/<uuid:order_uuid>/',
+         views.order_confirmation_view,
+         name='order_confirmation'),
 ]
