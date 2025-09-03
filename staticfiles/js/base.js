c91fde408481a0fc4f4173c5381b39ee23449af3
Morgana-S <st35giv9@students.codeinstitute.net>
2025-09-03 13:09:35 +0100

    Reformat async search js for better functionality

--- staticfiles/js/base.js
+++ staticfiles/js/base.js
@@ -15,43 +15,48 @@ function toolTipInit() {
 
 // Enables async search results
 function asyncSearch() {
+	let debounceTimer;
 	$('#search-input').on('input', function () {
-		var query = $(this).val();
+		clearTimeout(debounceTimer);
 
-		if (query.length < 2) {
-			$('#search-results').empty();
-			return;
-		}
+		debounceTimer = setTimeout(function () {
+			var query = $('#search-input').val();
+			if (query.length < 2) {
+				$('#search-results').empty();
+				return;
+			}
 
-		$.ajax({
-			url: '/async-search/',
-			data: { q: query },
-			dataType: 'json',
-			success: function (data) {
-				var resultsHtml = '';
-				if (data.length === 0) {
-					resultsHtml = '<div class="list-group-item"> No Results Found </div>';
-				} else {
-					data.forEach(function (item) {
-						if (item.type == 'record') {
-							resultsHtml += `<a href="/record/${item.slug}" class="list-group-item list-group-item-action">${item.name} <span class="ms-2 float-end badge rounded-pill bg-dark">Record</span></a>`;
-						} else {
-							resultsHtml += `<a href="/artist/${item.slug}" class="list-group-item list-group-item-action">${item.name}<span class="ms-2 float-end badge rounded-pill bg-success">Artist</span></a>`;
-						}
-					});
-				}
-				$('#search-results').html(resultsHtml);
-			},
-		});
+			$.ajax({
+				url: '/async-search/',
+				data: { q: query },
+				dataType: 'json',
+				success: function (data) {
+					var resultsHtml = '';
+					if (data.length === 0) {
+						resultsHtml =
+							'<div class="list-group-item"> No Results Found </div>';
+					} else {
+						data.forEach(function (item) {
+							if (item.type == 'record') {
+								resultsHtml += `<a href="/record/${item.slug}" class="list-group-item list-group-item-action">${item.name} <span class="ms-2 float-end badge rounded-pill bg-dark">Record</span></a>`;
+							} else {
+								resultsHtml += `<a href="/artist/${item.slug}" class="list-group-item list-group-item-action">${item.name}<span class="ms-2 float-end badge rounded-pill bg-success">Artist</span></a>`;
+							}
+						});
+					}
+					$('#search-results').html(resultsHtml);
+				},
+			});
+		}, 200);
 	});
 }
 
 // Close async search results when clicking outside the search input
-$(document).on('click', function(e) {
+$(document).on('click', function (e) {
 	if (
 		!$(e.target).closest('#search-input').length &&
 		!$(e.target).closest('#search-results').length
 	) {
 		$('#search-results').empty();
 	}
-})
+});
