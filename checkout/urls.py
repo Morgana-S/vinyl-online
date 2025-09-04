c643029d8db301cbfe8cdda4a5cf20ada95e172b
Morgana-S <st35giv9@students.codeinstitute.net>
2025-09-04 12:26:10 +0100

    Create url for checkout webhook

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . import views
+from . import views, webhook
 
 urlpatterns = [
     path('', views.checkout_view, name='checkout'),
     path('order/<uuid:order_uuid>/',
          views.order_confirmation_view,
          name='order_confirmation'),
+    path('stripe/webhook/', webhook.stripe_webhook, name='stripe_webhook')
 ]
