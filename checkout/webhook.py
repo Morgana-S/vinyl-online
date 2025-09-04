675ebc232e48abce4282ecdd43a59103c534aeb9
Morgana-S <st35giv9@students.codeinstitute.net>
2025-09-04 12:25:52 +0100

    Create webhook for handling paymentintents

---
+++ checkout/webhook.py
@@ -0,0 +1,42 @@
+from django.conf import settings
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+import stripe
+
+
+@require_POST
+@csrf_exempt
+def stripe_webhook(request):
+    """
+
+    """
+    payload = request.body
+    sig_header = request.META.get('HTTP_STRIPE_SIGNATURE')
+    endpoint_secret = settings.STRIPE_WEBHOOK_SECRET
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, endpoint_secret
+        )
+        print('Stripe signature header:', sig_header)
+    except ValueError:
+        # Invalid Payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError:
+        # Invalid Signature
+        return HttpResponse(status=400)
+
+    if event['type'] == 'payment_intent.succeeded':
+        payment_intent = event['data']['object']
+        handle_successful_payment(payment_intent)
+
+    return HttpResponse(status=200)
+
+
+def handle_checkout_session(session):
+    print('Checkout Completed:', session.get('id'))
+
+
+def handle_successful_payment(payment_intent):
+    print('Payment Succeeded:', payment_intent.get('id'))
