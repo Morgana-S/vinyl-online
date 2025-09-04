b44a24fb851ddf9184b128444e94550f1f395680
Morgana-S <st35giv9@students.codeinstitute.net>
2025-09-04 14:20:31 +0100

    Add urls for reviews

--- community/urls.py
+++ community/urls.py
@@ -7,6 +7,11 @@ urlpatterns = [
     path('support/ticket/<uuid:pk>',
          views.ticket_detail_view, name='ticket_detail'),
     path('who-we-are/', views.about_page_view, name='about'),
-    path('newsletter/', views.newsletter_subscribe_view, name='newsletter')
-
+    path('newsletter/', views.newsletter_subscribe_view, name='newsletter'),
+    path('add-review/<slug:record_slug>/',
+         views.add_review_view,
+         name='add_review'),
+     path('delete-review/<int:review_id>/',
+          views.delete_review_view, 
+          name='delete_review')
 ]
