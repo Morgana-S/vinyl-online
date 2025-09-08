018a40b69ed6e9c01cbd179659d942b14c06eb9b
Morgana-S <st35giv9@students.codeinstitute.net>
2025-09-08 12:32:25 +0100

    Change password requirements to 2 passwords when signing up

--- vinyl_online/settings.py
+++ vinyl_online/settings.py
@@ -149,7 +149,7 @@ AUTHENTICATION_BACKENDS = [
 
 LOGIN_REDIRECT_URL = '/'
 ACCOUNT_LOGOUT_REDIRECT_URL = '/'
-ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*', 'password1*']
+ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*', 'password1*', 'password2*']
 ACCOUNT_LOGIN_METHODS = {'username', 'email'}
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 
