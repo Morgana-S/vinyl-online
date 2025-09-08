260863dc53ceb0d4c38f74fd4da4c34ae4f82cfc
Morgana-S <st35giv9@students.codeinstitute.net>
2025-09-08 14:48:51 +0100

    Initialize defensive modal for remove all from basket

--- staticfiles/js/view_basket.js
+++ staticfiles/js/view_basket.js
@@ -4,4 +4,22 @@ document.addEventListener('DOMContentLoaded', function () {
 			input.form.submit();
 		});
 	});
+	removeModalInit();
 });
+
+
+function removeModalInit() {
+		const removeButtons = document.getElementsByClassName('remove-button');
+		const removeModal = new bootstrap.Modal(
+			document.getElementById('remove-from-basket-modal')
+		);
+		const removeForm = document.getElementById('remove-from-basket-form');
+
+		Array.from(removeButtons).forEach(button => {
+			button.addEventListener('click', function() {
+				const actionUrl = button.dataset.url;
+				removeForm.setAttribute('action', actionUrl);
+				removeModal.show();
+			})
+		})
+}
